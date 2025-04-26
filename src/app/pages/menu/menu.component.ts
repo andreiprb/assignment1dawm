1fecc7d582dc644cebadbef8759775293020965f
andreiprb <andreipriboi@icloud.com>
2025-04-26 19:11:37 +0300

    fixed error where menu page wouldn t allow for order now

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CoffeeListComponent } from '../../components/coffee-list/coffee-list.component';
@@ -11,7 +11,10 @@ import { CoffeeListComponent } from '../../components/coffee-list/coffee-list.co
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  @ViewChild('header') headerComponent!: HeaderComponent;
+
   handleCoffeeSelected(coffeeName: string) {
     console.log(`Selected coffee from menu: ${coffeeName}`);
+    this.headerComponent.updateSelectedCoffee(coffeeName);
   }
 }
